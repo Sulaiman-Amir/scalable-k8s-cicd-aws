c5a126c3be19c108d81c494f91be498a0ca4baf3
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-10 20:59:13 +0500

    Add main Terraform config for EKS cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,27 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = "topg-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.public[*].id
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy]
+}
 
