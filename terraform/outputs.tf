ab4e918c2bd54a38adccbf3f2aa4b68dff1c3b43
Sulaiman Amir <SulleMaan007@gmail.com>
2025-07-10 21:08:34 +0500

    Add Terraform outputs for EKS cluster name and AWS region

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,10 @@
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = var.cluster_name
+}
+
+output "aws_region" {
+  description = "The AWS region in which resources are deployed"
+  value       = var.aws_region
+}
 
